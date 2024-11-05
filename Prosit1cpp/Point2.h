04311942608e2935abaccda3886c0229652d50b5
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-05 15:25:39 +0100

    Update Point2.h
    ajout des classes

--- Prosit1cpp/Point2.h
+++ Prosit1cpp/Point2.h
@@ -7,6 +7,15 @@
 class Point2 {
 private:
   /*A COMPLETER*/
+  Point2 Point2_add(const Point2 *p, const Vector2 *r);
+  Point2 Point2_sub(Point2 *r, const Point2 *p);
 public:
   /*A COMPLETER*/
+  void Point2_init(Point2 *p);
+  void Point2_init_with_xy(Point2 *p, float x, float y);
+  float Point2_getX(const Point2 *p);
+  float Point2_getY(const Point2 *p);
+  void Point2_setX(Point2 *p, float x);
+  void Point2_setY(Point2 *p, float y);
+  void Point2_print(const Point2 *p);
 };
