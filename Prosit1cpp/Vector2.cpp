5a68a971be1173b726e0b4560ecf1e6517ef2821
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-05 15:27:20 +0100

    Update Vector2.cpp
    ajout des fonctions fu fichier C vers Cpp

--- Prosit1cpp/Vector2.cpp
+++ Prosit1cpp/Vector2.cpp
@@ -1,1 +1,38 @@
 #include "Vector2.h"
+
+void Vector2 :: Vector2_init(Vector2 *v, float x, float y) {
+    v->x = x;
+    v->y = y;
+}
+
+float Vector2 :: Vector2_getX(const Vector2 *v) {
+    return v->x;
+}
+
+float Vector2 :: Vector2_getY(const Vector2 *v) {
+    return v->y;
+}
+
+float Vector2 :: Vector2_norm(const Vector2 *v) {
+    return sqrt(v->x * v->x + v->y * v->y);
+}
+
+float Vector2 :: Vector2_dot(const Vector2 *v, const Vector2 *r) {
+    return v->x * r->x + v->y * r->y;
+}
+
+Vector2  :: Vector2_dot_scalar(const Vector2 *v, float u) {
+    Vector2 result;
+    Vector2_init(&result, u * v->x, u * v->y);
+    return result;
+}
+
+float Vector2 :: Vector2_cross(const Vector2 *v, const Vector2 *r) {
+    return (v->x * r->y) - (v->y * r->x);
+}
+
+Vector2 :: Vector2_negate(const Vector2 *v) {
+    Vector2 result;
+    Vector2_init(&result, -v->x, -v->y);
+    return result;
+}
