8f376d5997417d6bee340aa449eac6d58f018c38
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-07 20:19:29 +0100

    Update Vector2.h

--- Prosit1cpp/Vector2.h
+++ Prosit1cpp/Vector2.h
@@ -2,15 +2,19 @@
 
 #include <iostream>
 #include <math.h>
-
 float oa, ob;
 
 class Vector2 {
-private:
-  /*A COMPLETER*/
-public:
-  /*A COMPLETER*/
-    Vector2(int x, int y)/*: x(x), y(y) {}*/;
+    
+    private:
+/*A COMPLETER*/
+  
+    public:
+/*A COMPLETER*/
+
+    Vector2 (int x, int y): x(x), y(y) {}//Tentative de constructeur
+    //Vector2(int x, int y): x(x), y(y) {}
+    Vector2(int x, int y);
     //set_Vector2(int, int) ;
     //Vector2_cross();
 };
