ae420da2acb16abad12cd6dbbe763893577aaf6e
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-07 19:18:31 +0100

    Update main.cpp

--- Prosit1cpp/main.cpp
+++ Prosit1cpp/main.cpp
@@ -3,9 +3,11 @@
 #include <math.h>
 
 #include "Segment2.h"
+#include "Point2.h"
+#include "Vector2.h"
 
 void test() {
-  Point2 o1(1, 1), o2(4, 5), o3(2, 2), o4(3, 2), o10;
+  Point2 o1(1, 1), o2(4, 5), o3(2, 2), o4(3, 2), o10(0,0);
   Vector2 d1(3, 4), d2(3, 4), d3(1, 2), d4(1, 2);
   Segment2 s1(o1, d1), s2(o2, d2), s3(o3, d3), s4(o4, d4);
   IntersectionStatus status;
@@ -29,6 +31,3 @@ int main() {
 
   return EXIT_SUCCESS;
 }
-//Je test si ca marche
-
-// je retest
