ed9a2d7e5391a0cabe87ef31f383ca2a5650c524
Thaïs <125659463+thedarknessqueen@users.noreply.github.com>
2024-11-07 20:24:18 +0100

    Update Point2.h

--- Prosit1cpp/Point2.h
+++ Prosit1cpp/Point2.h
@@ -3,16 +3,18 @@
 #include "Vector2.h"
 #include <iostream>
 
-
-
 class Point2 {
-private:
-  /*A COMPLETER*/
-public:
-  /*A COMPLETER*/
-    Point2(int x, int y): x(x), y(y) {}
-    void Point2(int, int) ;
-    Point2_add() ;
-    float Point2_getX() ;
-    float Point2_getY() ;
+    
+    private:
+/*A COMPLETER*/
+
+    public:
+/*A COMPLETER*/
+
+    Point2 (int x, int y): x(x), y(y) {}//Tentative de constructeur
+    Point2(int x, int y);
+    //Point2(int, int) ;
+    //Point2_add() ;
+    float Point2_getX(float x) ;
+    float Point2_getY(float y) ;
 };
